b6116dbacef734decc87e934358508297b41d9a1
rojayamaharjan <work.work.mh3@gmail.com>
2022-01-13 15:01:21 -0600

    adding Thymeleaf

--- src/main/java/com/emrys/conference/config/AppConfig.java
+++ src/main/java/com/emrys/conference/config/AppConfig.java
@@ -1,5 +1,7 @@
 package com.emrys.conference.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
@@ -10,12 +12,36 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 
 import java.util.Locale;
 
 @Configuration
 public class AppConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+
+    @Bean
+    public SpringTemplateEngine templateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        templateEngine.setEnableSpringELCompiler(true);
+        return templateEngine;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver(){
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(applicationContext);
+        templateResolver.setPrefix("/WEB-INF/view/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setOrder(0);
+        return templateResolver;
+    }
+
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
@@ -46,10 +72,18 @@ public class AppConfig implements WebMvcConfigurer {
     @Bean
     public ViewResolver internalResourceViewResolver(){
         InternalResourceViewResolver bean = new InternalResourceViewResolver();
-        bean.setViewClass(JstlView.class);
+//        bean.setViewClass(JstlView.class);
         bean.setPrefix("/WEB-INF/jsp/");
         bean.setSuffix(".jsp");
-        bean.setOrder(0);
+        bean.setOrder(1);
         return bean;
+    }
+
+    @Bean
+    public ViewResolver thymeleafResolver(){
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(templateEngine());
+        thymeleafViewResolver.setOrder(0);
+        return thymeleafViewResolver;
     }
 }
--- src/main/java/com/emrys/conference/controllers/GreetingController.java
+++ src/main/java/com/emrys/conference/controllers/GreetingController.java
@@ -1,6 +1,7 @@
 package com.emrys.conference.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -10,12 +11,18 @@ import java.util.Map;
 public class GreetingController {
 
 
-    @RequestMapping(value ="/greeting", method = RequestMethod.GET)
+    @RequestMapping(value ="greeting", method = RequestMethod.GET)
     public String greeting(Map<String, Object> model){
 
         System.out.println("###########################");
         model.put("message","Hello Rojaya !");
         return "greeting";
+    }
+
+    @GetMapping("thyme")
+    public String thyme(Map<String, Object> model ){
+        model.put("message", "Hello Thyme!");
+        return "thyme";
     }
 
 }
