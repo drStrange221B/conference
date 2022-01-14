bb5301f3a7135ae8ec64eafbec8e8c949e937aa0
rojayamaharjan <work.work.mh3@gmail.com>
2022-01-13 22:33:54 -0600

    add Restful service and jquery

--- src/main/java/com/emrys/conference/controllers/RegistrationController.java
+++ src/main/java/com/emrys/conference/controllers/RegistrationController.java
@@ -2,9 +2,12 @@ package com.emrys.conference.controllers;
 
 import org.springframework.stereotype.Controller;
 import com.emrys.conference.model.Registration;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
@@ -17,7 +20,13 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute("registration") Registration registration){
+    public String addRegistration(@Valid @ModelAttribute("registration") Registration registration,
+                                  BindingResult result){
+        if(result.hasErrors())
+        {
+            System.out.printf("There were errors!");
+            return "registration";
+        }
 
         System.out.println("Registration Name: " + registration.getName());
 
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -12,11 +12,25 @@
 <html>
 <head>
     <title>Registration</title>
+    <style>
+        .error{
+            color:#ff0000;
+        }
+        .errorblock{
+            color: #000;
+            background-color: #ffEEEE;
+            border: 3px solid #ff0000;
+            padding: 8px;
+            margin: 16px;
+        }
+    </style>
 </head>
 <body>
 <h1>Registration</h1>
 
 <form:form modelAttribute="registration">
+    <form:errors path="*" cssClass="errorblock" element="div"/>
+
     <table>
     <tr>
         <td>
@@ -24,10 +38,11 @@
         </td>
         <td>
             <form:input path="name"></form:input>
+            <form:errors path="name" cssClass="error" element="div"/>
         </td>
     </tr>
     <tr>
-        <td colspan="2">
+        <td colspan="3">
             <input type="submit" value="<spring:message code = "save.changes" text="default"/>"/>
         </td>
     </tr>
